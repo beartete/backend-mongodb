e438ec82d4a2d9af869bbee2b06bb1ecde58eb3b
beartete <isabellacristinavieira2003@gmail.com>
2023-10-23 12:31:09 -0300

    aula 23-10

--- api/middleware/auth.js
+++ api/middleware/auth.js
@@ -1,22 +1,22 @@
 import jwt from 'jsonwebtoken'
 
-export default async function auth(req, res, next){
+export default async function auth(req, res, next) {
     const token = req.header('access-token') ||
                   req.headers['x-access-token']
-if(!token) return res.status(401).json({ //401 - Unauthorized
-     mensagem: 'Acesso negado. É obrigatório p envio do token JWT'
+    if(!token) return res.status(401).json({ //401 - Unauthorized
+        mensagem: 'Acesso negado. É obrigatório o envio do token JWT'
     })
     try {
         const decoded = jwt.verify(token, process.env.SECRET_KEY)
-        /* O decoded ira conter:
-        usuário (payload do usuário)
-        exp (expiratiom) - Data de expiração
-        iat (issued at) - Data de criação
+        /* O decoded irá conter: 
+            usuário (payload do usuario)
+            exp (expiration) - Data de expiração
+            iat (issued at) - Data de criação
         */
-       req.usuario + await decoded.usuario
+       req.usuario = await decoded.usuario
        next() //direcionamos para o endpoint
-    }   catch (e){
-        res.status(403).send({ error: `Token inválido: ${e.message}`})
+    } catch (e) {
+        res.status(403).send({error: `Token inválido: ${e.message}`})
         console.error(e.message)
-    }               
+    }
 }
--- api/routes/prestador.js
+++ api/routes/prestador.js
@@ -1,20 +1,21 @@
-/* API Rest dos prestadores */
-import express from "express";
-import { connectToDatabase } from "../utils/mongodb.js";
+import express from 'express'
+import { connectToDatabase } from '../utils/mongodb.js'
 import { check, validationResult } from 'express-validator'
- 
-const router = express.Router();
-const { db, ObjectId } = await connectToDatabase()
+
+const router = express.Router()
+const {db, ObjectId} = await connectToDatabase()
 const nomeCollection = 'prestadores'
- 
+
+import auth from '../middleware/auth.js'
+
 const validaPrestador = [
-    check("cnpj")
-    .not().isEmpty().trim().withMessage('é obirgatório informar o cnpj')
+    check('cnpj')
+    .not().isEmpty().trim().withMessage('É obrigatório informar o CNPJ')
     .isNumeric().withMessage('O CNPJ só deve conter números')
     .isLength({min: 14, max:14}).withMessage('O CNPJ deve conter 14 nºs'),
     check('razao_social')
-    .not().isEmpty().trim().withMessage('é obrigatório informar a razão')
-    .isAlphanumeric('pt-BR', {ignore: '/.'})
+    .not().isEmpty().trim().withMessage('É obrigatório informar a razão')
+    .isAlphanumeric('pt-BR', {ignore: '/. '})
     .withMessage('A razão social não deve ter caracteres especiais')
     .isLength({min: 5}).withMessage('A razão é muito curta. Mínimo 5')
     .isLength({max: 200}).withMessage('A razão é muito longa. Máximo 200'),
@@ -22,113 +23,123 @@ const validaPrestador = [
     .isNumeric().withMessage('O código do CNAE deve ser um número'),
     check('nome_fantasia').optional({nullable: true})
 ]
-/*
- * GET   /API/PRESTADORES
- * LISTA DE TODOS OS PRESTADORES
+
+/**
+ * GET /api/prestadores
+ * Lista todos os prestadores de serviço
  */
-router.get('/', async (req, res) => {
-    try {
-        db.collection(nomeCollection).find().sort({ razao_social: 1 })
-            .toArray((err, docs) => {
-                if (!err) {
-                    res.status(200).json(docs)
-                }
-            })
+
+router.get('/', auth, async (req, res)=> {
+    try{
+        db.collection(nomeCollection).find().sort({razao_social: 1}).toArray((err, docs) => {
+            if(!err){
+                res.status(200).json(docs)
+            }
+        })
     } catch (err) {
         res.status(500).json({
             errors: [{
-                value: `${err.message}`,
-                msg: `Erro ao obter a listagem dos prestadores`,
+                value: `&{err.message}`,
+                msg: 'erro ao obter a lista de prestadores',
                 param: '/'
             }]
         })
     }
 })
-router.get('/id/:id', async(req, res ) =>{
- 
+
+/**
+ * GET /api/prestadores/id/:id
+ * Lista todos os prestadores de serviço
+ */
+
+router.get('/id/:id', auth, async(req, res) => {
     try{
- 
-        db.collection(nomeCollection).find({'_id' : {$eq: ObjectId(req.params.id)}})
+        db.collection(nomeCollection).find({'_id': {$eq: ObjectId(req.params.id)}})
         .toArray((err, docs) => {
-            if(err){
-                res.status(400).json(err) //bad resquest
-            }else{
-                res.status(200).json(docs) //retorna documento
+            if(err) {
+                res.status(400).json(err) //bad request
+            } else {
+                res.status(200).json(docs) // retorna o documento
             }
-     })
-    }catch (err){
-        res.status(500).json({"erro" : err.message})
+        })
+    } catch (err) {
+        res.status(500).json({"error": err.message})
     }
 })
- 
 /**
- * DELETE /API/PRESTADORES/:ID
- * Apagar o prestador de serviço pelo id
+ * GET /api/prestadores/razao/:razao
+ * Lista os prestadores de serviço pela razao social
  */
- 
-router.delete('/:id', async(req, res) => {
+
+router.get('/razao/:razao', auth, async(req, res) => {
+    try{
+        db.collection(nomeCollection)
+        .find({'razao_social': {$regex: req.params.razao, $options: "i"}})
+        .toArray((err, docs) => {
+            if(err) {
+                res.status(400).json(err) //bad request
+            } else {
+                res.status(200).json(docs) // retorna o documento
+            }
+        })
+    } catch (err) {
+        res.status(500).json({"error": err.message})
+    }
+})
+
+/**
+ * DELETE /api/prestadores/id/:id
+ * Apaga o prestador de serviço pelo id
+ */
+
+router.delete('/:id', auth,  async(req, res) => {
     await db.collection(nomeCollection)
-    .deleteOne({"_id": { $eq: ObjectId(req.params.id)}})
+    .deleteOne({"_id" : {$eq: ObjectId(req.params.id)}})
     .then(result => res.status(200).send(result))
     .catch(err => res.status(400).json(err))
 })
+
 /**
- * POST /API/PRESTADORES/:ID
+ * POST /api/prestadores
  * Insere um novo prestador de serviço
  */
-router.post('/', validaPrestador, async(req, res) =>{
+router.post('/', auth, validaPrestador, async(req,res) => {
+
     const errors = validationResult(req)
+
     if(!errors.isEmpty()){
         return res.status(400).json(({
             errors: errors.array()
         }))
-    } else {
+    }else {
         await db.collection(nomeCollection)
         .insertOne(req.body)
         .then(result => res.status(200).send(result))
-        .catche(err => res.status(400).json(err))
+        .catch(err => res.status(400).json(err))
     }
 })
+
 /**
- * put /API/PRESTADORES/:ID
- * altera um prestador de serviço
+ * PUT /api/prestadores
+ * Altera um prestador de serviço
  */
- 
-router.put('/', validaPrestador, async(req, res) =>{
-    let idDocumento = req.body._id // armazena o id do documento
+router.put('/', auth, validaPrestador,  async(req,res) => {
+
+    let idDocumento = req.body._id //armazenando o id do documento
     delete req.body._id //iremos remover o id do body
     const errors = validationResult(req)
+
     if(!errors.isEmpty()){
         return res.status(400).json(({
             errors: errors.array()
         }))
-    } else {
+    }else {
         await db.collection(nomeCollection)
-        .updateOne({'_id': {$eq : ObjectId(idDocumento)}},
-                { $set: req.body})
+        .insertOne(req.body)
         .then(result => res.status(200).send(result))
-        .catche(err => res.status(400).json(err))
+        .catch(err => res.status(400).json(err))
     }
 })
- 
-// razão social
-// listagem pela razao
- 
- 
-router.get('/razao/:razao', async(req, res ) =>{
-    try{
-        db.collection(nomeCollection)
-        .find({'razao_social' : {$regex:req.params.razao, $options : "i"}})
-        .toArray((err, docs) => {
-            if(err){
-                res.status(400).json(err) //bad resquest
-            }else{
-                res.status(200).json(docs) //retorna documento
-            }
-     })
-    }catch (err){
-        res.status(500).json({"erro" : err.message})
-    }
-})
- 
+
+
 export default router
